9a8b526f06d8436456157241a23e07107d6c5564
adamFoldvari <foldadam93@gmail.com>
2017-06-18 17:25:24 +0200

    Add basic functionality

---
+++ files/js/main.js
@@ -0,0 +1,38 @@
+var buttons = document.querySelectorAll('button')
+var operators = ['%', '*', '/', '+', '-', '(', ')']
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function(e) {
+        var screen = document.querySelector('p');
+        var screenValue = screen.innerHTML;
+        var buttonValue = this.innerHTML;
+
+        if (buttonValue == 'C') {
+            screen.innerHTML = ""; 
+        }
+        else if (buttonValue == '='){
+            screen.innerHTML = eval(screen.innerHTML);
+        }
+        else if (operators.indexOf(buttonValue) > -1) {
+            var lastCharacter = screenValue[screenValue.length-1];
+            
+            if (screenValue == '' && buttonValue == '-') {
+                screen.innerHTML = screen.innerHTML + buttonValue;
+            }
+
+            else if (screenValue != '' && operators.indexOf(lastCharacter) == -1) {
+                screen.innerHTML = screen.innerHTML + buttonValue;
+            }
+
+            else if(screenValue != '' && operators.indexOf(lastCharacter) > -1) {
+                screen.innerHTML = screenValue.replace(/.$/, buttonValue);
+            }
+            
+        
+          }
+        else {
+            screen.innerHTML = screen.innerHTML + buttonValue
+        }
+
+    }
+}
