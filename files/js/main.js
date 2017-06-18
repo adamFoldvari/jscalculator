179117916965047fd4fc0eebee3472485c56d76f
adamFoldvari <foldadam93@gmail.com>
2017-06-18 17:57:30 +0200

    Add validation for zero division and empty evaluation

--- files/js/main.js
+++ files/js/main.js
@@ -1,17 +1,37 @@
 var buttons = document.querySelectorAll('button')
 var operators = ['%', '*', '/', '+', '-', '(', ')']
+var wasEval = false;
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function(e) {
         var screen = document.querySelector('p');
         var screenValue = screen.innerHTML;
         var buttonValue = this.innerHTML;
+        
+        if (wasEval) {
+            screen.innerHTML = '';
+            wasEval = false;
+        }
 
         if (buttonValue == 'C') {
             screen.innerHTML = ""; 
         }
         else if (buttonValue == '='){
-            screen.innerHTML = eval(screen.innerHTML);
+            var evaluation = eval(screen.innerHTML);
+
+            if (evaluation == 'Infinity') {
+                screen.innerHTML = 'Error: zero divison'
+            }
+            else if(evaluation == undefined) {
+                screen.innerHTML = 'Enter an operation'
+            }
+            else {
+                screen.innerHTML = eval(screen.innerHTML);
+            }
+            wasEval = true;
+        }
+        else if (buttonValue == 'A'){
+            screen.innerHTML = screen.innerHTML
         }
         else if (operators.indexOf(buttonValue) > -1) {
             var lastCharacter = screenValue[screenValue.length-1];
